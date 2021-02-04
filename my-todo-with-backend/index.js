cdf11ccf81b1733b65220f09128183488d3e85c0
Jan Vondrouš <j.vondrous@quadient.com>
2021-02-04 21:14:43 +0100

    Server is serving html.

---
+++ my-todo-with-backend/index.js
@@ -0,0 +1,107 @@
+'use strict';
+
+// MODEL
+class Model {
+    todos = new Map() //map is not best if we want change order of todos, but i want try to use Map
+
+    addTodo = (todo) => {
+        this.todos.set(todo.id, todo)
+    }
+
+    removeTodo = (id) => {
+        this.todos.delete(id)
+    }
+
+}
+
+class Todo {
+    constructor(text) {
+        this.id = generateId()
+        this.text = text
+    }
+}
+
+// STORAGE
+class Storage {
+    saveModel = (model) => {
+        const value = JSON.stringify(Array.from(model.todos.entries())); // ouch :(
+        localStorage.setItem('todos', value)
+    }
+
+    loadModel = () => {
+        const model = new Model()
+        const todosString = localStorage.getItem('todos');
+        try {
+            const todos = JSON.parse(todosString)
+                model.todos = new Map(todos)
+        } catch (e) {
+            console.error(e)
+            model.addTodo(new Todo("Koupit Jablka"))
+            model.addTodo(new Todo("Vyluxovat"))
+            model.addTodo(new Todo("Zalít kytky"))
+        }
+        return model
+    }
+}
+
+// APPLICATION
+const storage = new Storage()
+
+const model = storage.loadModel()
+
+function saveModel() {
+    storage.saveModel(model)
+}
+
+function render() {
+
+    const todosDiv = document.getElementById("todos")
+    todosDiv.innerHTML = ""
+
+    model.todos.forEach((todo) => {
+            const todoDiv = document.createElement("div")
+
+            const textSpan = document.createElement("span")
+            textSpan.textContent = todo.text
+            todoDiv.appendChild(textSpan)
+
+            const removeButton = document.createElement("button")
+            removeButton.setAttribute("onclick", `removeTodo("${todo.id}")`)
+            removeButton.textContent = "delete"
+            todoDiv.appendChild(removeButton)
+        
+            todosDiv.appendChild(todoDiv)
+        }
+    )
+
+
+}
+
+
+function addNewTodo() {
+    model.addTodo(createNewTodo())
+    render()
+    saveModel()
+}
+
+function createNewTodo() {
+    const newTodoTextDiv = document.getElementById("todoText");
+    const text = newTodoTextDiv.value
+    newTodoTextDiv.value = ""
+    return new Todo(text)
+}
+
+
+function removeTodo(id) {
+    model.removeTodo(id)
+    render()
+    saveModel()
+}
+
+
+function generateId() {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
--- my-todo-with-backend/project.js
+++ my-todo-with-backend/project.js
@@ -1,19 +1,35 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-
-// const requestListener = function (req, res) {
-//     fs.readFile(__dirname + "/index.html")
-//         .then(contents => {
-//             res.setHeader("Content-Type", "text/html");
-//             res.writeHead(200);
-//             res.end(contents);
-//         })
-// };
-
 const requestListener = function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("My first server!");
+    switch (req.url) {
+        case "/":
+        case "/index.html":
+            fs.readFile(__dirname + "/index.html")
+                .then(contents => {
+                    res.setHeader("Content-Type", "text/html");
+                    res.writeHead(200);
+                    res.end(contents);
+                })
+            break
+        case "/index.js":
+            fs.readFile(__dirname + "/index.js")
+                .then(contents => {
+                    res.setHeader("Content-Type", "text/javascript");
+                    res.writeHead(200);
+                    res.end(contents);
+                })
+            break
+        case "/authors":
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error: "Resource not found"}));
+    }
+
+
 };
 
 http.createServer(requestListener).listen(9393);
