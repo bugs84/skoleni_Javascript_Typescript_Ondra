7f47bb204f13e056380e5af0a8b0d3be6f011fc9
Jan Vondrouš <j.vondrous@quadient.com>
2021-02-04 15:29:54 +0100

    version 2.0 - removing TodoApp with model

--- index.js
+++ index.js
@@ -74,36 +74,6 @@ function generateId() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-// function addNewTodo() {
-//
-//
-//
-//     const todoDiv = document.createElement("div")
-//     todoDiv.setAttribute("class", "todo")
-//
-//     const todoTextDiv = document.getElementById("todoText")
-//
-//     // todoDiv.innerHTML = todoTextDiv.value
-//
-//     todoDiv.innerHTML = `
-//     <span>${todoTextDiv.value}</span><button onclick="removeTodo(this)">delete</button>
-//     `
-//
-//
-//
-//
-//     todoTextDiv.value = ""
-//
-//     document.getElementById("todos").appendChild(todoDiv)
-//
-// }
-//
-// function removeTodo(source) {
-//     // alert('delete' + source)
-//     // source.closest(".todo").remove
-//     findAncestor(source, "todo").remove()
-// }
-
 
 function findAncestor(el, cls) {
     while ((el = el.parentElement) && !el.classList.contains(cls)) ;
