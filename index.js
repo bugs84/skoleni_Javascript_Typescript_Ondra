6b71b27242384e8b102b5ca7b0614b4c9673837b
Jan Vondrouš <j.vondrous@quadient.com>
2021-02-04 14:49:16 +0100

    first working example - reviewed by ondra

--- index.js
+++ index.js
@@ -5,19 +5,33 @@ function addNewTodo() {
 
 
     const todoDiv = document.createElement("div")
-    // todoDiv.setAttribute("class", "todo")
+    todoDiv.setAttribute("class", "todo")
 
     const todoTextDiv = document.getElementById("todoText")
 
-    todoDiv.innerHTML =todoTextDiv.value
+    // todoDiv.innerHTML = todoTextDiv.value
 
-    // todoTextDiv.value = ""
+    todoDiv.innerHTML = `
+    <span>${todoTextDiv.value}</span><button onclick="removeTodo(this)">delete</button>
+    `
 
-
+    
+    
+    
+    todoTextDiv.value = ""
+    
     document.getElementById("todos").appendChild(todoDiv)
 
 }
 
-// function addNewTodo(remove) {
-//
-// }
+function removeTodo(source) {
+    // alert('delete' + source)
+    // source.closest(".todo").remove
+    findAncestor(source, "todo").remove()
+}
+
+
+function findAncestor (el, cls) {
+    while ((el = el.parentElement) && !el.classList.contains(cls));
+    return el;
+}
